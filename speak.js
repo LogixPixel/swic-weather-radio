9616c08adacbff5468e263dbb37856339278081e
Ashleigh Timmons <74683653+log1xx@users.noreply.github.com>
2022-06-28 23:31:09 -0400

    Added cycle

--- speak.js
+++ speak.js
@@ -1,7 +1,13 @@
-const say = require('say')
+const say = require('async-sayjs');
 
 function speak(text) {
-  say.speak(text);
+  fs.readFile(text, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    say.speak(data);
+  });
 }
 
 module.exports = { speak };
